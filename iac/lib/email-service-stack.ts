54083f301691f94fd9df0a1a9ceee0c54d188f98
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-11-13 09:48:46 +1000

    Added CDK IAC

---
+++ iac/lib/email-service-stack.ts
@@ -0,0 +1,37 @@
+import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns";
+import * as cdk from "@aws-cdk/core";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ec2 from "@aws-cdk/aws-ec2";
+
+export class EmailServiceStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const defaultVpc = ec2.Vpc.fromLookup(this, "DefaultVpc", {
+      isDefault: true,
+    });
+
+    const fargateService = new ApplicationLoadBalancedFargateService(
+      this,
+      "EmailFargateService",
+      {
+        cpu: 512,
+        vpc: defaultVpc,
+        memoryLimitMiB: 1024,
+        desiredCount: 1,
+        taskImageOptions: {
+          containerPort: 8080,
+          image: ecs.ContainerImage.fromAsset("../"),
+          environment: {
+            SENDGRID_API_KEY: process.env.SENDGRID_API_KEY!,
+            MAILGUN_API_KEY: process.env.MAILGUN_API_KEY!,
+          },
+        },
+      }
+    );
+
+    fargateService.targetGroup.configureHealthCheck({
+      path: "/actuator/health",
+    });
+  }
+}
