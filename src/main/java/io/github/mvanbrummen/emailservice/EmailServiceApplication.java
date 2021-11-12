aa89f668c8a7b8f47ad52410309e8e00d856fe47
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-11-13 08:29:59 +1000

    Added spring retry for email provider fallback

--- src/main/java/io/github/mvanbrummen/emailservice/EmailServiceApplication.java
+++ src/main/java/io/github/mvanbrummen/emailservice/EmailServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.mvanbrummen.emailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/io/github/mvanbrummen/emailservice/service/EmailService.java
+++ src/main/java/io/github/mvanbrummen/emailservice/service/EmailService.java
@@ -3,16 +3,27 @@ package io.github.mvanbrummen.emailservice.service;
 import io.github.mvanbrummen.emailservice.api.EmailSendRequest;
 import io.github.mvanbrummen.emailservice.gateway.EmailGateway;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private final EmailGateway sendgridEmailGateway;
     private final EmailGateway mailgunEmailGateway;
 
+    @Retryable(maxAttempts = 1)
     public void sendEmail(final EmailSendRequest emailSendRequest) {
         mailgunEmailGateway.sendEmail(emailSendRequest);
     }
 
+    @Recover
+    public void sendEmailFallback(final Exception e, final EmailSendRequest emailSendRequest) {
+        log.debug("Falling back to secondary email provider", e);
+
+        sendgridEmailGateway.sendEmail(emailSendRequest);
+    }
 }
