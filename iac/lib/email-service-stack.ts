f2ad4f1f643c55e758b6290cc212aa22e419706b
Michael Van Brummen <michaelvanbrummen@gmail.com>
2021-11-13 11:56:36 +1000

    Moved app directory

--- iac/lib/email-service-stack.ts
+++ iac/lib/email-service-stack.ts
@@ -2,6 +2,7 @@ import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns
 import * as cdk from "@aws-cdk/core";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ec2 from "@aws-cdk/aws-ec2";
+import { FargatePlatformVersion } from "@aws-cdk/aws-ecs";
 
 export class EmailServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -19,9 +20,11 @@ export class EmailServiceStack extends cdk.Stack {
         vpc: defaultVpc,
         memoryLimitMiB: 1024,
         desiredCount: 1,
+        assignPublicIp: true,
+        platformVersion: FargatePlatformVersion.VERSION1_3,
         taskImageOptions: {
           containerPort: 8080,
-          image: ecs.ContainerImage.fromAsset("../docker"),
+          image: ecs.ContainerImage.fromAsset("../email-service"),
           environment: {
             SENDGRID_API_KEY: process.env.SENDGRID_API_KEY!,
             MAILGUN_API_KEY: process.env.MAILGUN_API_KEY!,
